d97034dbc0219d4ae3db93ea440a5bd64d810caf
dobidof90 <dobidof90@gmail.com>
2018-12-17 22:18:09 +0900

    Print sample calendar

--- src/dobidof90/calendar/Calendar.java
+++ src/dobidof90/calendar/Calendar.java
@@ -33,7 +33,7 @@ public class Calendar {
 			if (month > 12 || month < 1) {
 				continue;
 			}
-			System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
+			cal.printSampleCalendar();
 		}
 		System.out.printf("Have a nice day!\n");
 		scanner.close();
