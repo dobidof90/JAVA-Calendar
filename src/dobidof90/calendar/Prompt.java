8bbb448b5b2da294242c3c6960e6685bb7155228
dobidof90 <dobidof90@gmail.com>
2018-12-17 22:54:09 +0900

    Add Prompt class

---
+++ src/dobidof90/calendar/Prompt.java
@@ -0,0 +1,32 @@
+package dobidof90.calendar;
+
+import java.util.Scanner;
+
+public class Prompt {
+
+	public void runPrompt() {
+		Scanner scanner = new Scanner(System.in);
+		Calendar cal = new Calendar();
+
+		while (true) {
+			System.out.printf("월을 입력하세요.\n");
+			System.out.printf("> ");
+			int month = scanner.nextInt();
+			if (month == -1) {
+				break;
+			}
+			if (month > 12 || month < 1) {
+				continue;
+			}
+			cal.printCalendar(2018, month);
+		}
+		System.out.printf("Have a nice day!\n");
+		scanner.close();
+	}
+
+	public static void main(String[] args) {
+		Prompt p = new Prompt();
+		p.runPrompt();
+	}
+
+}
