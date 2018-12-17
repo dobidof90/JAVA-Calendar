3414a47580d7d2b7f0eaa2f374cbc9aec72b848d
dobidof90 <dobidof90@gmail.com>
2018-12-17 22:53:44 +0900

    Modify printCalendar function

--- src/dobidof90/calendar/Calendar.java
+++ src/dobidof90/calendar/Calendar.java
@@ -10,33 +10,19 @@ public class Calendar {
 		return MAX_DAYS[month - 1];
 	}
 
-	public void printSampleCalendar() {
-		System.out.println("  일    월    화    수    목    금    토");
-		System.out.println("--------------------");
-		System.out.println(" 1  2  3  4  5  6  7");
-		System.out.println(" 8  9 10 11 12 13 14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");
-	}
-
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		Calendar cal = new Calendar();
-
-		while (true) {
-			System.out.printf("월을 입력하세요.\n");
-			System.out.printf("> ");
-			int month = scanner.nextInt();
-			if (month == -1) {
-				break;
+	public void printCalendar(int year, int month) {
+		System.out.printf("      <<%4d년 %3d월>>\n", year, month);
+		System.out.println(" SU MO TU WE TH FR SA");
+		System.out.println("---------------------");
+		int maxDay = getMaxDaysOfMonth(month);
+		
+		for(int i = 1; i <= maxDay; i++) {
+			System.out.printf("%3d", i);
+			if(i % 7 == 0) {
+				System.out.println();
 			}
-			if (month > 12 || month < 1) {
-				continue;
-			}
-			cal.printSampleCalendar();
 		}
-		System.out.printf("Have a nice day!\n");
-		scanner.close();
+		System.out.println();
 	}
 
 }
