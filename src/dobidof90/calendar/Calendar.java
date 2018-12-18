bd01f8b11ad1aaf091a9d5c18dc03b11cb08eabd
dobidof90 <dobidof90@gmail.com>
2018-12-19 01:10:18 +0900

    Add auto searching code at Calendar class

--- src/dobidof90/calendar/Calendar.java
+++ src/dobidof90/calendar/Calendar.java
@@ -1,11 +1,37 @@
 package dobidof90.calendar;
 
-import java.util.Scanner;
-
 public class Calendar {
 
 	private final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	private final int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private final int[] WEEKDAY = { 0, 3, 0, 3, 2, 3, 2, 3, 3, 2, 3, 2 };
+	private final int[] LEAP_WEEKDAY = { 0, 3, 1, 3, 2, 3, 2, 3, 3, 2, 3, 2 };
+
+	public int firstDayOfYear(int year) {
+		int a = 1;
+		for (int i = 1900; i < year; i++) {
+			if (isLeapYear(i)) {
+				a += 2;
+			} else {
+				a++;
+			}
+		}
+		return (a % 7);
+	}
+
+	public int firstDayOfMonth(int year, int month) {
+		int a = firstDayOfYear(year);
+		if (isLeapYear(year)) {
+			for (int i = 0; i < month; i++) {
+				a += LEAP_WEEKDAY[i];
+			}
+		} else {
+			for (int i = 0; i < month; i++) {
+				a += WEEKDAY[i];
+			}
+		}
+		return (a % 7);
+	}
 
 	public boolean isLeapYear(int year) {
 		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
@@ -21,11 +47,13 @@ public class Calendar {
 			return MAX_DAYS[month - 1];
 	}
 
-	public void printCalendar(int year, int month, int weekday) {
+	public void printCalendar(int year, int month) {
 		System.out.printf("     <<%4d년 %3d월>>\n", year, month);
 		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println("---------------------");
 		int maxDay = getMaxDaysOfMonth(year, month);
+
+		int weekday = firstDayOfMonth(year, month);
 
 		for (int i = 0; i < weekday; i++) {
 			System.out.printf("   ");
