1726edf8e28331ea0da9967f4dfd25967b0d39d9
dobidof90 <dobidof90@gmail.com>
2018-12-19 00:11:18 +0900

    Add weekday at Calendar class

--- src/dobidof90/calendar/Calendar.java
+++ src/dobidof90/calendar/Calendar.java
@@ -21,15 +21,18 @@ public class Calendar {
 			return MAX_DAYS[month - 1];
 	}
 
-	public void printCalendar(int year, int month) {
-		System.out.printf("      <<%4d년 %3d월>>\n", year, month);
+	public void printCalendar(int year, int month, int weekday) {
+		System.out.printf("     <<%4d년 %3d월>>\n", year, month);
 		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println("---------------------");
 		int maxDay = getMaxDaysOfMonth(year, month);
 
+		for (int i = 0; i < weekday; i++) {
+			System.out.printf("   ");
+		}
 		for (int i = 1; i <= maxDay; i++) {
 			System.out.printf("%3d", i);
-			if (i % 7 == 0) {
+			if ((i + weekday) % 7 == 0) {
 				System.out.println();
 			}
 		}
